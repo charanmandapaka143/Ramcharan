6412d139e8e049e49e2f7f9f47fcd137ad02bc09
charanmandapaka143 <ramc71131@gmail.com>
2025-12-31 15:26:27 +0530

    Add files via upload

---
+++ assignment3/assignment3.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+
+// The main function is where program execution starts
+int main() {
+
+    // Print the name on a new line
+    printf("Name: Alex\n");
+
+    // Print the age on a new line
+    printf("Age: 20\n");
+
+    // Print the country on a new line
+    printf("Country: India\n");
+
+    // Return 0 to indicate successful program execution
+    return 0;
+}
